ae3220deeff8f5c39c6a97572d38c00bf7cd4102
Hyesop <shinhyesop@gmail.com>
2022-05-26 09:31:33 +0100

    Update agent.jl

--- agent.jl
+++ agent.jl
@@ -16,5 +16,20 @@ Schelling
 space
 properties = Dict(:min_to_be_happy => 3)
 
-model = AgentBasedModel(Schelling, space; properties) 
+scheduler = Schedulers.randomly
 
+model = AgentBasedModel(Schelling, space; properties, scheduler) 
+
+using Random # for reproducibility
+function initialize(; N = 320, M = 20, min_to_be_happy = 3, seed = 125)
+    space = GridSpace((M, M), periodic = false)
+    properties = Dict(:min_to_be_happy => min_to_be_happy)
+    rng = Random.MersenneTwister(seed)
+    
+    
+    
+    model = ABM(
+        SchellingAgent, space;
+        properties, rng, scheduler = Schedulers.randomly
+    )
+end
